0938abd8e3c6c91138da30b933cdc19530253da9
rofrischmann <robin@rofrischmann.de>
2016-05-26 19:06:10 +0200

    1.0.0 initial

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# unitless-css-property
-Helper to check for unitless CSS property names
+# Unitless CSS Property
+
+Provides a simple helper to check for unitless CSS property names. It also checks for prefixed property names.
+
+# Usage
+```sh
+npm i --save unitless-css-property
+```
+
+```javascript
+import isUnitlessCSSProperty from 'unitless-css-property'
+
+isUnitlessCSSProperty('lineHeight') // => true
+isUnitlessCSSProperty('WebkitFlex') // => true
+isUnitlessCSSProperty('width') // => false
+```
---
+++ index.js
@@ -0,0 +1,55 @@
+var unitlessProperties = {
+  animationIterationCount: true,
+  borderImageOutset: true,
+  borderImageSlice: true,
+  borderImageWidth: true,
+  boxFlex: true,
+  boxFlexGroup: true,
+  boxOrdinalGroup: true,
+  columnCount: true,
+  flex: true,
+  flexGrow: true,
+  flexPositive: true,
+  flexShrink: true,
+  flexNegative: true,
+  flexOrder: true,
+  gridRow: true,
+  gridColumn: true,
+  fontWeight: true,
+  lineClamp: true,
+  lineHeight: true,
+  opacity: true,
+  order: true,
+  orphans: true,
+  tabSize: true,
+  widows: true,
+  zIndex: true,
+  zoom: true,
+
+  // SVG-related properties
+  fillOpacity: true,
+  floodOpacity: true,
+  stopOpacity: true,
+  strokeDasharray: true,
+  strokeDashoffset: true,
+  strokeMiterlimit: true,
+  strokeOpacity: true,
+  strokeWidth: true
+}
+
+var prefixes = ['Webkit', 'ms', 'Moz', 'O']
+
+function getPrefixedKey(prefix, key) {
+  return prefix + key.charAt(0).toUpperCase() + key.slice(1)
+}
+
+// add all prefixed properties as well
+Object.keys(unitlessProperties).forEach(function(property) {
+  prefixes.forEach(function(prefix) {
+    unitlessProperties[getPrefixedKey(prefix, property)] = true
+  })
+})
+
+module.exports = function(property) {
+  return unitlessProperty[property]
+}
